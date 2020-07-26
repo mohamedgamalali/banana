6c2e0d46d615fb890d63c3a698bd993717522a6f
mohamedgamalali <mg7404809@gmail.com>
2020-07-27 00:31:47 +0200

    make it better

--- routes/admin/shop.js
+++ routes/admin/shop.js
@@ -2,11 +2,12 @@ const express      = require('express');
 const {body}       = require('express-validator');
 
 const shopController = require('../../controllers/admin/shop');
+const isAuth         = require('../../meddlewere/admin/isAuth');
 
 
 const router  = express.Router();
 
 
-router.get('/products/:catigoryId',shopController.getProducts);                                   
+router.get('/products/:catigoryId',isAuth,shopController.getProducts);                                   
 
 module.exports = router;
