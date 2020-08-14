425fa593f9a7bc5040b6c8191be827052a7773a5
mohamedgamalali <mg7404809@gmail.com>
2020-08-14 02:44:18 +0200

    make it better

--- controllers/client/support.js
+++ controllers/client/support.js
@@ -3,6 +3,8 @@ const { validationResult } = require('express-validator');
 const Order = require('../../models/order');
 const Issue = require('../../models/issues');
 
+const deleteFile = require("../../helpers/file");
+
 exports.postIssue = async (req, res, next) => {
 
     const orderId = req.body.orderId;
@@ -10,7 +12,7 @@ exports.postIssue = async (req, res, next) => {
     const demands = req.body.demands;
     const image = req.files;
     const errors = validationResult(req);
-    let imageUrl = [] ;
+    let imageUrl = [];
     try {
         if (!errors.isEmpty()) {
             const error = new Error(`validation faild for ${errors.array()[0].param} in ${errors.array()[0].location}`);
@@ -25,13 +27,13 @@ exports.postIssue = async (req, res, next) => {
             throw error;
         }
         const order = await Order.findById(orderId);
-        if(!order){
+        if (!order) {
             const error = new Error(`order not found`);
             error.statusCode = 404;
             error.state = 9;
             throw error;
         }
-        if(order.status != 'started'){
+        if (order.status != 'started') {
             const error = new Error(`wanna put issue for not ended order`);
             error.statusCode = 422;
             error.state = 14;
@@ -43,25 +45,33 @@ exports.postIssue = async (req, res, next) => {
 
         const issue = new Issue({
             client: req.userId,
-            order:order._id,
-            reason:reason,
-            demands:demands,
-            imageUrl:imageUrl
+            order: order._id,
+            reason: reason,
+            demands: demands,
+            imageUrl: imageUrl
         });
-       const i =  await issue.save();
-        
+        const i = await issue.save();
+
         //seller id must be added
 
         res.status(201).json({
-            state:1,
-            issue:i,
-            message:'issue created'
+            state: 1,
+            issue: i,
+            message: 'issue created'
         });
 
     } catch (err) {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
+        //image must be deleted
+        if (image.length > 0) {
+            image.forEach(i => {
+                deleteFile.deleteFile(path.join(__dirname + '/../../' + i.path));
+            });
+        }
+        err.state = image[0].path;
+
         next(err);
     }
 }
