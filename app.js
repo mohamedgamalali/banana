caf412f8ad17f4fa87425c922f83d61a9e19c53e
mohamedgamalali <mg7404809@gmail.com>
2020-09-02 14:25:01 +0200

    make it better

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const erorrMeddlewere = require('./helpers/errors');
 app.use('/client', router.client.auth, router.client.shop, router.client.user, router.client.support);
 app.use('/client/guest', router.client.guest);
 app.use('/seller', router.seller.auth, router.seller.shop, router.seller.user, router.seller.support);
-app.use('/admin', router.admin.auth, router.admin.shop, router.admin.support);
+app.use('/admin', router.admin.auth, router.admin.shop, router.admin.user,router.admin.support);
 
 //error handle meddlewere
 app.use(erorrMeddlewere);
